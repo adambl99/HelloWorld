1bd5edcf008f4dfacd8e1e4c7a79db891040b0f5
Adaml <adam6925@stud.kea.dk>
2021-09-14 12:53:54 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        System.out.println("Hvad er klokken?");
+        System.out.println("Klokken er ved at være 14");
+
+    }
+
